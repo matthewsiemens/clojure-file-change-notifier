d06fa36d9b2c232cd822f62651ff2d25582d01f8
Matthew Siemens <contact@matthewsiemens.com>
2016-04-04 19:15:03 -0600

    Update license info

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Send email with list of modified files in directory"
   :url "https://github.com/matthewsiemens/clojure-file-change-notifier"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-time "0.11.0"]
                  [com.draines/postal "1.11.4"]])
